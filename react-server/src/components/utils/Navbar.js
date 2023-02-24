f88dd3137d0478d245556639f566e329f6f75178
Jay Liang <jayliang789@gmail.com>
2023-02-23 21:58:43 -0500

    added searchbar

--- react-server/src/components/utils/Navbar.js
+++ react-server/src/components/utils/Navbar.js
@@ -1,3 +1,5 @@
+import SearchBar from "./SearchBar";
+
 const Navbar = () =>{
     return(
         <div className="p-5 bg-sky-300 shadow md:flex md:items-center md:justify-between">
@@ -6,6 +8,9 @@ const Navbar = () =>{
                 <a href="/">Fitcheck</a>
                 
             </span>
+            <div className="w-80">
+                <SearchBar/>
+            </div>
             <ul className="md:flex md:items-center z-[-1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7">
                 <li className="mx-4 my-6 md:my-0">
                   <a href="/login" className="text-xl hover:text-cyan duration-500">Login</a>
