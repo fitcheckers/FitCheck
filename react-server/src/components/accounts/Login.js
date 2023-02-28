eebd62f92a5b0a9843c3ce002c6814d5017f59b3
David Chen <dav@Davids-MacBook-Pro.local>
2023-02-27 20:53:26 -0500

    Changes to logo and navbar

--- react-server/src/components/accounts/Login.js
+++ react-server/src/components/accounts/Login.js
@@ -47,10 +47,10 @@ export default function Login() {
   }
 
   return (
-    <div className="min-h-full flex items-center justify-center py-36 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <div>
-          <h2 className="mt-4 text-3xl text-center tracking-tight font-light dark:text-white">
+          <h2 className="mt-24 text-3xl text-center tracking-tight font-light dark:text-white">
             Login to your account
           </h2>
         </div>
--- react-server/src/components/accounts/Register.js
+++ react-server/src/components/accounts/Register.js
@@ -48,10 +48,10 @@ export default function Register() {
     }
 
   return (
-    <div className="min-h-full flex items-center justify-center py-36 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <div>
-          <h2 className="mt-4 text-3xl text-center tracking-tight font-light dark:text-white">
+          <h2 className="mt-24 text-3xl text-center tracking-tight font-light dark:text-white">
             Register your account
           </h2>
         </div>
