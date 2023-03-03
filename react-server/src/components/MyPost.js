81b0a17ed7f91b61f01472705c09a8e03b48efb1
David Chen <dav@Davids-MacBook-Pro.local>
2023-03-03 00:12:58 -0500

    made changes to mypost and connected it to edit profile

--- react-server/src/components/MyPost.js
+++ react-server/src/components/MyPost.js
@@ -5,6 +5,8 @@ import Pin from "./Pin.js";
 import Modal from "./Modal";
 import Fab from "@mui/material/Fab";
 import AddIcon from "@mui/icons-material/Add";
+import { HiOutlineSquares2X2 } from "react-icons/hi2";
+import picture from "./accounts/profile.webp";
 
 class MyPost extends Component {
   constructor(props) {
@@ -28,32 +30,43 @@ class MyPost extends Component {
       };
     });
   };
-
+  
+  
   render() {
     return (
-      <div>
-        <div className="fixed bottom-0 right-0 z-1 ">
-          <Fab
-            onClick={() => this.setState({ show_modal: true })}
-            color="primary"
-            aria-label="add"
-            size="large"
-          >
-            <AddIcon />
-          </Fab>
+      <div className="overflow-x-hidden">
+        <div>
+          <img className="w-48 relative top-32 left-24 rounded-full" src={picture} alt="profile"></img>
+          <p className="relative left-80 text-lg font-bold">@username</p>
+          <p className="relative left-80 text-lg">2 Post</p>
+          <a href="/Profile" className="relative left-80 bg-gray-300 pt-1 pb-1 pl-5 pr-5 rounded-full hover:bg-gray-500">Edit Profile</a>
+          <HiOutlineSquares2X2 size="32" className="relative left-24 top-16" />
+          <p className="relative left-32 top-9 font-extrabold text-xl">My Posts</p>
         </div>
+        <div>
+          <div className="fixed bottom-0 right-0 z-1 ">
+            <Fab
+              onClick={() => this.setState({ show_modal: true })}
+              color="primary"
+              aria-label="add"
+              size="large"
+            >
+              <AddIcon />
+            </Fab>
+          </div>
 
-        <div className="pin_container">{this.state.pins}</div>
+          <div className="pin_container">{this.state.pins}</div>
 
-        <div
-          onClick={(event) =>
-            event.target.className === "add_pin_modal"
-              ? this.setState({ show_modal: false })
-              : null
-          }
-          className="add_pin_modal_container"
-        >
-          {this.state.show_modal ? <Modal add_pin={this.add_pin} /> : null}
+          <div
+            onClick={(event) =>
+              event.target.className === "add_pin_modal"
+                ? this.setState({ show_modal: false })
+                : null
+            }
+            className="add_pin_modal_container"
+          >
+            {this.state.show_modal ? <Modal add_pin={this.add_pin} /> : null}
+          </div>
         </div>
       </div>
     );
