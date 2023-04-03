a49c4fb169b51623d5bb853f0ec9ba9ea0980dcd
Jay Liang <jayliang789@gmail.com>
2023-04-03 13:15:54 -0400

    changed comment bar

--- react-server/src/components/posts/PostModal.js
+++ react-server/src/components/posts/PostModal.js
@@ -14,17 +14,13 @@ class PostModal extends Component {
                 <div className="fixed overflow-y-auto">
                     <div className='fixed bg-[#D9D9D9] top-[5%] left-[14%] w-[75%] h-[90%] rounded-3xl inline-block'> 
                         <TfiArrowLeft onClick={() => toggleModal()} size={28} className="relative left-2 top-2 cursor-pointer"/>
-                        <img className='relative object-cover object-center top-[.5%] left-[6%] h-[81%] w-[34%] rounded-tl-2xl rounded-bl-2xl drop-shadow-lg border-white border-y-4 border-l-4' src={image_url} alt="user's post"></img>
-                        <div className='fixed bg-white h-[70%] w-[39%] top-[12%] left-[44%] -z-10 rounded-tr-2xl rounded-br-2xl break-all'>
-                            <div className='fixed border-b-2 border-gray-200 bg-white  h-[13%] w-[39%] top-[10%] left-[44%] rounded-tr-2xl z-10'></div>
-                            <img className="fixed h-16 w-16 object-cover rounded-full top-[12%] left-[45%] z-20" src={user_pfp || profile} alt="user's pfp"></img>
-                            <div className='fixed left-[60%] sm:left-[58%] md:left-[55%] top-[100px] font-bold text-xl z-20'>{user_name}</div>
+                        <img className='fixed object-cover object-center top-[10%] left-[19%] h-[72%] w-[25%] rounded-tl-2xl rounded-bl-2xl drop-shadow-lg border-white border-y-4 border-l-4' src={image_url} alt="user's post"></img>
+                        <div className='fixed bg-white h-[70%] w-[39%] top-[12%] left-[44%] -z-10 rounded-tr-2xl rounded-br-2xl flex flex-col object-cover'>
+                            <div className='fixed border-b-2 border-gray-200 bg-white  h-[11%] w-[39%] top-[10%] left-[44%] rounded-tr-2xl z-10'></div>
+                            <a className='fixed left-[50%] top-[14%] font-bold text-xl z-20'><img className="fixed h-16 w-16 object-cover rounded-full top-[12%] left-[45%] z-20 whitespace-normal" src={user_pfp || profile} alt="user's pfp"></img>{user_name}</a>
                             <div className='relative left-10 sm:left-12 top-28 w-[60%] sm:w-[72%] md:w-[86%] font-bold text-2xl'>{title}</div>
                             <div className='relative left-10 sm:left-12 top-36 font-light'>{description}</div>
-                            <form className='relative left-10 sm:left-12 top-[70%] bg-white border-black border-2 rounded-2xl h-[50px] w-[60%]'>
-                                <input className='w-[100%] h-full rounded-2xl bg-inherit border-white' placeholder='Add a comment'></input>
-                            
-                            </form>
+                            <input className='relative left-10 top-[79%] w-[50%] h-[50px] bg-gray-50 border-inherit pl-2' placeholder='Add a comment...'></input>
                         </div>
                     </div>
                 </div>
