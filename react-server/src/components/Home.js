aead7ee0e4bd3e6dba526b4c9a1bfd5756197794
David Chen <dav@Davids-MacBook-Pro.local>
2023-04-05 13:03:05 -0400

    Made some changes to homepage

--- react-server/src/components/Home.js
+++ react-server/src/components/Home.js
@@ -1,43 +1,70 @@
 import React, { Component } from "react";
+import { useState } from "react";
 import Box from "@mui/material/Box";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import { maxHeight, maxWidth } from "@mui/system";
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import IconButton from '@mui/material/IconButton';
+import InfoIcon from '@mui/icons-material/Info';
+
 import Pin from "./Pin.js";
 import axios from "axios";
 import { FetchPost } from "./MyPost.js";
-class HomePage extends Component {
+
+
+function printName(title){
+  console.log(title);
+}
+
+function HomePage(){
+  const [show, setShow] = useState(false);
+  const [hoveredItem, setHoveredItem] = useState(null);
   //get all posts in collection
   // convert them into pins object and store them in pin array
   // render them into pin container
 
-  render() {
-    return (
-      <Box
-        sx={{
-          width: maxWidth,
-          height: maxHeight,
-          paddingLeft: 12,
-          paddingRight: 4,
-          paddingTop: 15,
-        }}
-      >
-        <ImageList variant="masonry" cols={5} gap={25}>
-          {itemData.map((item) => (
-            <ImageListItem key={item.img}>
+  return (
+    <Box
+      sx={{
+        width: maxWidth,
+        height: maxHeight,
+        paddingLeft: 12,
+        paddingRight: 4,
+        paddingTop: 15,
+      }}
+    >
+      <ImageList variant="masonry" cols={5} gap={25}>
+        {itemData.map((item) => (
+          <ImageListItem key={item.img}
+          onMouseOver={() => setHoveredItem(item)}
+          onMouseOut={() => setHoveredItem(null)}>
               <img
                 src={`${item.img}?w=248&fit=crop&auto=format`}
                 srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
                 alt={item.title}
                 loading="lazy"
                 style={{ borderRadius: 20 }}
+                onClick={() => printName(item.title)} 
               />
-            </ImageListItem>
-          ))}
-        </ImageList>
-      </Box>
-    );
-  }
+              {hoveredItem === item && ( <ImageListItemBar
+                className="rounded-b-2xl"
+                title={item.title}
+                subtitle={item.author}
+                actionIcon={
+                  <IconButton
+                    sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                    aria-label={`info about ${item.title}`}
+                  >
+                    <InfoIcon />
+                  </IconButton>
+                }
+              />)}
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </Box>
+  );
 }
 
 //fetch post title, desp img frm firstore post
