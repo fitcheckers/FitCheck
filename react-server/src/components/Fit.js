fefb6ed7a0e317af839a6522b3b01f355fe2608d
David Chen <dav@Davids-MBP.lan>
2023-05-07 18:16:34 -0400

    Recommendation Page Working

--- react-server/src/components/Fit.js
+++ react-server/src/components/Fit.js
@@ -1,18 +1,184 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import PostModal from "./posts/PostModal.js";
+import axios from "axios";
+import { BsHeartFill } from "react-icons/bs";
+import { useAuth } from "../contexts/AuthContext.js";
+import { useState, useEffect } from "react";
+import { maxHeight, maxWidth } from "@mui/system";
 
 function Fit() {
-  return(
-    <div className="pt-[90px] pl-[70px] pr-[10px] w-[100%] h-screen flex flex-col gap-3 -z-10">
-      <div className="top-52 w-full h-[10%] break-words rounded-lg shadow-md shadow-gray-600 text-white pl-2 pt-2 text-4xl bg-gradient-to-r from-[#005668] to-[#101827]">Footwear</div>
-      <div className="top-52 w-full h-[10%] break-words rounded-lg shadow-md shadow-gray-600 text-white pl-2 pt-2 text-4xl bg-gradient-to-r from-[#A86A3C] via-[#B98559] to-[#E8C091]">Top</div>
-      <div className="top-52 w-full h-[10%] break-words rounded-lg shadow-md shadow-gray-600 text-white pl-2 pt-2 text-4xl bg-gradient-to-r from-[#A1C4FD] to-[#C2E9FB]">Bottom</div>
-      <div className="top-52 w-full h-[10%] break-words rounded-lg shadow-md shadow-gray-600 text-white pl-2 pt-2 text-4xl bg-gradient-to-r from-[#09203F] to-[#537895]">Footwear</div>
-      <div className="top-52 w-full h-[10%] break-words rounded-lg shadow-md shadow-gray-600 text-white pl-2 pt-2 text-4xl bg-gradient-to-r from-[#A9F1DF] to-[#FFBBBB]">Footwear</div>
-      <div className="top-52 w-full h-[10%] break-words rounded-lg shadow-md shadow-gray-600 text-white pl-2 pt-2 text-4xl bg-gradient-to-r from-[#4E65FF] to-[#92EFFD]">Footwear</div>
-      <div className="top-52 w-full h-[10%] break-words rounded-lg shadow-md shadow-gray-600 text-white pl-2 pt-2 text-4xl bg-gradient-to-r from-[#D8B5FF] to-[#1EAE98]">Footwear</div>
-      <div className="top-52 w-full h-[10%] break-words rounded-lg shadow-md shadow-gray-600 text-white pl-2 pt-2 text-4xl bg-gradient-to-r from-[#614385] to-[#516395]">Footwear</div>
-      <div className="top-52 w-full h-[10%] break-words rounded-lg shadow-md shadow-gray-600 text-white pl-2 pt-2 text-4xl bg-gradient-to-r from-[#2E3192] to-[#1BFFFF]">Footwear</div>
-      <div className="top-52 w-full h-[10%] break-words rounded-lg shadow-md shadow-gray-600 text-white pl-2 pt-2 text-4xl bg-[#d4d4d4]">Footwear</div>
+  const [hoveredItem, setHoveredItem] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedItem, setSelectedItem] = useState("");
+  const [selectedItemProfile, setSelectedItemProfile] = useState("");
+  const [postData, setPostData] = useState([]);
+  const [userData, setUserData] = useState("");
+  const { currentUser, setError } = useAuth();
+ 
+  async function getPost(array) {
+    try {
+      console.log(array);
+      const response = await axios.post("http://localhost:80/posts/query", { tags: array });
+      console.log(response);
+      const data = response.data.map((post) => {
+        if(post.likes.includes(currentUser.uid))
+        {
+          return {...post, isLiked: true, numLikes: post.likes.length};
+        } else {
+          return {...post, isLiked: false, numLikes: post.likes.length};
+        }
+      })
+      console.log(data);
+      return data;
+    } catch (e) {
+      console.log(e);
+      console.log("Error from calling axios from Home");
+    }
+  }
+
+  async function getUser(user_id) {
+    try {
+      const response = await axios.post("http://localhost:80/users/get", {
+        id: user_id,
+      });
+      console.log("Calling axios from getUser MyPost");
+      return response.data;
+    } catch (e) {
+      console.log(e);
+      console.log("Error from calling axios from getUser MyPost");
+    }
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      const fetchedData = await getUser(currentUser.uid);
+      setUserData(fetchedData);
+      const data = await getPost(fetchedData.styles);
+      setPostData(data);
+    }
+    fetchData();
+  }, []);
+
+  const handlePinClick = (item) => {
+    if(currentUser)
+    {
+      setShowModal(true);
+      setSelectedItem(item);
+      setSelectedItemProfile(item.user);
+    }
+    else
+    {
+      setError("Must be logged in to access");
+    }
+  };
+
+  const handleLikeClick = async (e) => {
+    console.log(e);
+    console.log(currentUser.uid);
+    const userObject = {
+      user_id: currentUser.uid,
+      post_id: e,
+    }
+    let value;
+    setPostData((data) =>
+      data.map((post) => {
+        if (post.id === e) {
+          value = post.isLiked;
+          return {...post, isLiked: !post.isLiked};
+        }
+        return post;
+    }));
+    if(value)
+    {
+      console.log(value);
+      try{
+        const response = await axios.post("http://localhost:80/post/unlike", userObject);
+        console.log(response);
+        setPostData((data) =>
+          data.map((post) => {
+            if (post.id === e) {
+              return {...post, numLikes: post.numLikes-1};
+            }
+            return post;
+        }));
+      } catch (e){
+          console.log(e);
+      }
+    }
+    else
+    {
+      console.log(value);
+      try{
+        const response = await axios.post("http://localhost:80/post/like", userObject);
+        console.log(response);
+        setPostData((data) =>
+          data.map((post) => {
+            if (post.id === e) {
+              return {...post, numLikes: post.numLikes+1};
+            }
+            return post;
+        }));
+      } catch (e){
+          console.log(e);
+      }
+    }
+  }
+
+  return (
+    <div>
+      <PostModal post={selectedItem}
+          user={selectedItemProfile}
+          isOpen={showModal}
+          toggleModal={() => setShowModal(false)} />
+      <Box
+        sx={{
+          width: maxWidth,
+          height: maxHeight,
+          paddingLeft: 12,
+          paddingRight: 4,
+          paddingTop: 15,
+        }}
+      >
+        <ImageList variant="woven" cols={5} gap={25}>
+          {postData.map((item) => (
+            <ImageListItem key={item.id}
+            onMouseOver={() => setHoveredItem(item)}
+            onMouseOut={() => setHoveredItem(null)}>
+                <img
+                  src={`${item.image_url}?w=248&fit=crop&auto=format`}
+                  srcSet={`${item.image_url}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                  alt={item.title}
+                  loading="lazy"
+                  style={{ borderRadius: 20 }}
+                  onClick={() => handlePinClick(item)} 
+                />
+                {hoveredItem === item && ( <ImageListItemBar
+                  className="rounded-b-2xl"
+                  title={item.title}
+                  subtitle={item.author}
+                  actionIcon={
+                    <>
+                      <button
+                        style={{
+                          color: "red",
+                        }}
+                        className="pint_mock_icon_container mr-2"
+                        onClick={() => handleLikeClick(item.id)}
+                      >
+                        <BsHeartFill className="text-white" sx={{'&:hover': {cursor: 'pointer'}}} color={item.isLiked ? 'red' : 'grey'}/>
+                        {item.numLikes}
+                      </button>
+                    </>
+                  }
+                />)}
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </Box>
     </div>
   );
 }
