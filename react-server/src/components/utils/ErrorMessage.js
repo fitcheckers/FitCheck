1696d248e432900f20be5f7ff7f6cfe0aeb9fb1c
David Chen <dav@Davids-MBP.lan>
2023-05-15 11:11:08 -0400

    Error Message Size

--- react-server/src/components/utils/ErrorMessage.js
+++ react-server/src/components/utils/ErrorMessage.js
@@ -7,7 +7,7 @@ export default function ErrorMessage() {
     return (
       error && (
         <div className="fixed left-[30%] justify-center z-50">
-          <div className="fixed rounded-md w-[25%] left-[40%] bg-red-50 h-[5%] min-h-[50px] p-4 z-20 top-[3%] overflow-hidden hover:overflow-y-auto">
+          <div className="fixed rounded-md w-[25%] left-[40%] bg-red-50 h-[6%] min-h-[60px] p-4 z-20 top-[3%] overflow-hidden hover:overflow-y-auto">
             <div className="flex">
               <div className="flex-shrink-0">
                 <XCircleIcon
